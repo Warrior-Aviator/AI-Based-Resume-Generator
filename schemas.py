aa42b880d642000cf0395ae2209a60e49889747a
DHRUVA PARASHAR <84895023+Warrior-Aviator@users.noreply.github.com>
2025-09-05 16:56:26 +0530

    Remove Old Files.

--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class ResumeRequest(BaseModel):
-    resume_text: str
-    job_description: str
-
-class DownloadRequest(BaseModel):
-    resume_text: str
-    cover_letter: str
